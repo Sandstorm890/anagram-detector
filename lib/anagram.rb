e52333867a4ffec1a452cf1cefc57ffdcd9a70ee
zaynegoering <zaynegoering@gmail.com>
2020-10-29 15:14:51 -0700

    commit tree 8c1826e35b4f82375eb83a6b89653e3d1bb57086

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,46 @@
-# Your code goes here!
+require "pry"
+
+class Anagram
+    def initialize(word)
+        @word = word
+    end
+
+    def match(anagrams)
+        counter = 0
+        new_array = []
+        matches = []
+        if anagrams.kind_of?(String)
+            while counter < anagrams.split(" ").length
+            
+                
+                anagrams.split(" ").each do |item|
+                    if @word.split(//).sort == item.split(//).sort
+                        matches << item
+                        counter += 1
+                    else
+                        counter += 1
+                        next
+                    end
+                end
+            end
+        elsif anagrams.kind_of?(Array)
+            while counter < anagrams.length
+                
+                
+                anagrams.each do |item|    
+                    if @word.split(//).sort == item.split(//).sort
+                        
+                        matches << item
+                        counter += 1
+                    else
+                        counter += 1
+                        next
+                    end
+                end
+            end
+        else
+            puts "Invalid format"
+        end
+        matches     
+    end
+end
